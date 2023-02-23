ff3ac82f7037b9055673629e5b6e02bb52638aa8
piyush singh <pk.rv.668@gamil.com>
2023-02-24 00:12:32 +0530

    day3_fw19_1048

--- quickdeal/src/Piyush/HomePage.jsx
+++ quickdeal/src/Piyush/HomePage.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import "./HomePage.css"
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css';
+import { Box } from '@chakra-ui/react';
+import img1 from "./img1.png"
+import img2 from "./img2.png"
+import img3 from "./img3.png"
+import { BiChevronRight } from "react-icons/bi";
 
 let sliderArrary = [
     {
@@ -8,12 +15,12 @@ let sliderArrary = [
         link: "link",
     },
     {
-        img: "https://teja9.kuikr.com/escrow/assets/image/certified_landing_page/other-furnitures.png",
+        img: "https://teja10.kuikr.com/escrow/assets/image/assured_landing_page/all_electonics_desktop.png",
         title: "title",
         link: "link",
     },
     {
-        img: "https://teja9.kuikr.com/escrow/assets/image/certified_landing_page/other-furnitures.png",
+        img: "https://teja10.kuikr.com/escrow/assets/image/assured_landing_page/sofa_sets_desktop.png",
         title: "title",
         link: "link",
     },
@@ -78,34 +85,205 @@ let sliderArrary = [
         link: "link",
     }
 ]
-
+const responsive = {
+    
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+// https://teja10.kuikr.com/escrow/assets/image/quikrBazaar/MT_links_bg.png
+//https://teja10.kuikr.com/escrow/assets/image/quikrBazaar/HL_links_bg.png
+//https://teja10.kuikr.com/escrow/assets/image/quikrBazaar/EL_links_bg.png
 function HomePage() {
-    const imgNum=0
+    
     return (
         <div id="P_HompageContainer">
             <div className='P_div_Title'>
                 <h1 className='P_Title'> Most Popular Products</h1>
             </div>
 
-            <div className= 'P_slider_div'>
-            <div className= 'P_slider'>
-            {sliderArrary.map((el) => {
+{/*  
+            <Carousel responsive={responsive} 
+  dotListClass="custom-dot-list-style"
+  itemClass="carousel-item-padding-40-px"
+  containerClass="carousel-container"
+    >
+    {sliderArrary.map((el) => {
                     return (
-                        <div className='P_slider_div_array'>
-                            <a href={el.link}>
-                                <div className='p_image'>  <img src={el.img} /></div>
+                         <>                     
+                                <a href={el.link}>
+                            <div className='P_image_div'>  <div className='p_image'>  <img src={el.img} /></div></div>
+                              
                                
                             </a>
                             <p>{el.title}</p>
-                        </div>
+                            </>
+
                     )
                 })}
+</Carousel> */}
+
+
+          <div>
+          <h1 className='P_Title_2'> Buy and Sell from 1200+ Cities Across India</h1>
+          </div>
+            
+             <div  className='P_card'>
+             
+              <div className='p_div_card' >
+              <div className='p_img_div'>
+                <img className='p_img' src={img1} alt="" />
+              </div>
+              <ul style={{padding:"0px 15px"}}>
+                <li>
+                    <a href="">Used Mobiles
+                        <i><BiChevronRight/></i>
+                    </a>
+                </li>
+             
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              </ul>
+                  
             </div>
+            <div className='p_div_card'>
+              <div className='p_img_div'>
+                <img className='p_img' src={img2} alt="" />
+              </div>
+              <ul style={{padding:"0px 15px"}}>
+                <li>
+                    <a href="">Used Mobiles
+                        <i><BiChevronRight/></i>
+                    </a>
+                </li>
+             
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+                 <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              </ul>
+                  
+            </div>
+            <div className='p_div_card'>
+              <div className='p_img_div'>
+                <img className='p_img' src={img3} alt="" />
+              </div>
+              <ul style={{padding:"0px 15px"}}>
+                <li>
+                    <a href="">Used Mobiles
+                        <i><BiChevronRight/></i>
+                    </a>
+                </li>
+             
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="">Used Mobiles
+                          <i><BiChevronRight/></i>
+                    </a>
+                </li>
+              </ul>
+                  
+            </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+         
                 
             </div>
-            <div className='P_nav_btn P_btn_next'></div>
-            <div className='P_nav_btn P_btn_prev'></div>
-        </div>
+           
+      
     )
 }
 
