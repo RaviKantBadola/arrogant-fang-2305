1b6dd036da8a3f64001ec6081db1c95079abd122
Manju145 <manjuyadav0514001@gmail.com>
2023-02-24 22:58:48 +0530

    project

--- quickdeal/src/redux/product.api.js
+++
@@ -1,8 +0,0 @@
-// API calling function hereby
-
-import axios from "axios"
-
-export const getProductsAPI = async () => {
-    let response = await axios.get("https://courageous-umbrella-moth.cyclic.app/products");
-    return response.data;
-}
