56357a26a96ed3cb536ca4ac5283d6f1313bf6d9
Manju145 <manjuyadav0514001@gmail.com>
2023-02-24 22:53:33 +0530

    project

--- quickdeal/src/App.js
+++ quickdeal/src/App.js
@@ -2,17 +2,11 @@
 import './App.css';
 
 import Products from './RaviComponent/Products';
-
 import Product from './product/product';
-n
-
-
 import logo from './logo.svg';
 import AdminNavbar from './Admin/AdminComponents/AdminNavbar';
 
 
-
-import './App.css';
 
 import HomePage from './Piyush/HomePage';
 
@@ -26,6 +20,8 @@ function App() {
     {/* <Navbar/> */}
       <AdminNavbar />
 
+    </div>
+    
     </div>
   );
 }
