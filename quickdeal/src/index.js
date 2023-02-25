3bc2ba79d614d16d8546076c5ffc8f1f7d0fa50c
Manju145 <manjuyadav0514001@gmail.com>
2023-02-25 09:40:52 +0530

    pro

--- quickdeal/src/index.js
+++ quickdeal/src/index.js
@@ -1,12 +1,12 @@
  
  
-   
- 
+   import React from 'react';
+   import ReactDOM from 'react-dom/client';
  import App from "./App"
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
+import reportWebVitals from './reportWebVitals';
 import { store } from './RaviComponent/Products-Redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
