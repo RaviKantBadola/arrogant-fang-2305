908bab9a5cc27d2166487c5c79a1e2d668220fa6
Manju145 <manjuyadav0514001@gmail.com>
2023-02-25 22:27:07 +0530

    project

--- quickdeal/src/index.js
+++ quickdeal/src/index.js
@@ -6,7 +6,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-} from './RaviComponent/Products-Redux/store';
+// } from './RaviComponent/Products-Redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
