8e926895f4a0d398ae19287f71f1afdca5ed3553
RaviKantBadola <107493045+RaviKantBadola@users.noreply.github.com>
2023-02-26 10:29:57 +0530

    day-5

--- quickdeal/src/App.js
+++ quickdeal/src/App.js
@@ -1,18 +1,12 @@
 
 import './App.css';
- 
-import Navbar from './Component/Navbar';
 import AllRoutes from './Route/AllRoutes';
- 
-
-import Products from './RaviComponent/Products';
-
 function App() {
   return (
 
     <div className="App" style={{width:"80%",margin:"auto",height:"auto"}}>
- 
-
+      
+      <AllRoutes/>
     </div>
  
    
