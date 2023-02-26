f31424c5904af5c5342de45250e9ee13f5c98391
Manju145 <manjuyadav0514001@gmail.com>
2023-02-26 14:14:08 +0530

    project

--- quickdeal/src/index.js
+++ quickdeal/src/index.js
@@ -18,9 +18,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 
-
-
-=======
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
